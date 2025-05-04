a4b92a2514b62862436a793e04cece8ea9690396
Safrayter <leshkasa@gmail.com>
2025-05-04 15:28:09 +0300

    tsk_1_2_2
    style
    create_room.py

--- src/core/pages/admin_page/create_room.py
+++ src/core/pages/admin_page/create_room.py
@@ -1,7 +1,6 @@
 import random
 import time
 
-
 from src.core.locators.admin_page.locators import AdminPageLocators
 from src.core.pages.base_page import BasePage
 
@@ -10,15 +9,15 @@ class AdminPageCreateRoom(BasePage):
     locators = AdminPageLocators
 
     def admin_page_create_room(self):
-           self.element_is_visible(self.locators.INPUT_NUMBER_OF_ROOM).send_keys(random.randint(1, 999))
-           self.open_type_of_room_drop_down()
-           self.element_is_visible(self.locators.TYPE_OF_ROOM_TWIN).click()
-           self.element_is_visible(self.locators.SELECT_ACCESSIBLE_TRUE).click()
-           self.element_is_visible(self.locators.INPUT_PRICE).send_keys(random.randint(1, 999))
-           self.element_is_visible(self.locators.WIFI_CHECKBOX).click()
-           self.element_is_visible(self.locators.TV_CHECKBOX_TEXT).click()
-           self.element_is_visible(self.locators.CREATE_ROOM).click()
-           time.sleep(5)
+        self.element_is_visible(self.locators.INPUT_NUMBER_OF_ROOM).send_keys(random.randint(1, 999))
+        self.open_type_of_room_drop_down()
+        self.element_is_visible(self.locators.TYPE_OF_ROOM_TWIN).click()
+        self.element_is_visible(self.locators.SELECT_ACCESSIBLE_TRUE).click()
+        self.element_is_visible(self.locators.INPUT_PRICE).send_keys(random.randint(1, 999))
+        self.element_is_visible(self.locators.WIFI_CHECKBOX).click()
+        self.element_is_visible(self.locators.TV_CHECKBOX_TEXT).click()
+        self.element_is_visible(self.locators.CREATE_ROOM).click()
+        time.sleep(5)
 
     def open_type_of_room_drop_down(self):
         self.element_is_visible(self.locators.TYPE_OF_ROOM).click()
