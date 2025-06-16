b98d47c8966252d67897246edaec94a073696933
Safrayter <leshkasa@gmail.com>
2025-06-16 22:34:32 +0300

    refactor: improve logging config in config_logger.py
    - Rewrote log path handling using pathlib.Path
    - Added support for LOG_PATH env variable from docker-compose.yml
    - Defaults to ../../logs if LOG_PATH is not provided

--- config/logger/config_logger.py
+++ config/logger/config_logger.py
@@ -1,10 +1,14 @@
 import os
+from pathlib import Path
 from loguru import logger
 
 logger.remove()  # remove logging from console
 
-log_dir = os.path.join(os.path.dirname(__file__),'..','..','logs', 'logs.log')
-logger.add(log_dir,
+log_dir = Path(os.getenv("LOG_PATH", Path(__file__).resolve().parent.parent.parent / "logs"))
+log_dir.mkdir(parents=True, exist_ok=True)
+
+log_file = log_dir / "logs.log"
+logger.add(log_file,
            rotation='10 MB',
            mode='w',
            level='DEBUG')
