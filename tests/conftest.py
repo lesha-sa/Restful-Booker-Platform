d12a623ce3210ae83358c751cdc60975127455b8
Safrayter <leshkasa@gmail.com>
2025-06-16 22:41:13 +0300

    feat: add remote Selenium support with SELENIUM_URL - Added support for remote WebDriver via SELENIUM_URL environment variable. If the variable is not set, local Chrome is started using webdriver_manager. Also applied --start-maximized argument via Options for the browser.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,20 @@
+import os
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import  Service as ChromeService
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
+
 
 @pytest.fixture(scope='function')
 def driver():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    driver.maximize_window()
+    selenium_url = os.getenv('SELENIUM_URL')
+    options = Options()
+    options.add_argument("--start-maximized")
+    if selenium_url:
+        driver = webdriver.Remote(command_executor=selenium_url, options=options)
+    else:
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
     yield driver
     driver.quit()
