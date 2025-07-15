ad573fd3cc80fe98e475ebebbf588aa15ba64857
Safrayter <leshkasa@gmail.com>
2025-07-16 00:50:27 +0300

    refactor(tests): move conftest.py to tests root for shared fixture access and update driver fixture with auto-detection of local or remote Selenium

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,32 @@
 import os
 import pytest
+import socket
+from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
+load_dotenv()
+
+
+def is_resolvable(host: str) -> bool:
+    try:
+        socket.gethostbyname(host)
+        return True
+    except:
+        return False
+
 @pytest.fixture(scope='function')
 def driver():
     selenium_url = os.getenv('SELENIUM_URL')
     options = Options()
     options.add_argument("--start-maximized")
-    if selenium_url:
+
+    host = selenium_url.split("//")[-1].split(":")[0] if selenium_url else None
+
+    if selenium_url and host and is_resolvable(host):
         driver = webdriver.Remote(command_executor=selenium_url, options=options)
     else:
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
