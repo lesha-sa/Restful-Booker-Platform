81357f1059bf41fc0135558f1e3d5bc0b57e3f94
Safrayter <leshkasa@gmail.com>
2025-08-15 01:40:59 +0300

    refactor: update get_token method

--- src/api/get_auth_token.py
+++ src/api/get_auth_token.py
@@ -1,9 +1,29 @@
 import httpx
+from config.logger.config_logger import get_logger
 
-def get_token():
-    response = httpx.post(
-        'https://automationintesting.online/api/auth/login',
-        json={"username": "admin", "password": "password"}
-    )
-    assert response.status_code == 200
-    return response.json()["token"]
+logger = get_logger()
+
+
+AUTH_URL = "https://automationintesting.online/api/auth/login"
+AUTH_PAYLOAD = {
+    "username": "admin",
+    "password": "password"
+}
+TIMEOUT = 10.0
+
+def get_token() -> str:
+    """Receives an authorization token from the server."""
+    try:
+        response = httpx.post(
+            AUTH_URL,
+            json = AUTH_PAYLOAD,
+            timeout=TIMEOUT
+        )
+        response.raise_for_status()
+        token = response.json().get("token")
+        assert token, "Token not found in response"
+        logger.info("Token successfully received")
+        return token
+    except Exception as e:
+        logger.error("Unable to obtain token: {}", e)
+        raise
